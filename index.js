82376453d4e09b2538dbde59d2260fa70e5c65f2
nan-yb <dydqls5757@gmail.com>
2022-02-07 11:25:45 +0900

    [CORS 테스트]-01

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const formidable = require("express-formidable");
 const cors = require("cors");
+
+require("dotenv").config();
+
 const {
   article,
   user,
@@ -11,13 +14,18 @@ const {
   common,
 } = require("./router");
 const app = express();
-const PORT = 8080;
 const SECRET = "@askdasklF!@#123dnasdnkas";
 
 //파일업로드 Multipart/form-data
 const multer = require("multer");
 
-app.use(cors());
+let corsOptions = {
+  origin: "https://sub.ybyblog.com",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
@@ -35,10 +43,19 @@ app.use(common);
 
 app.use(formidable());
 
+var deployType = `${process.env.DEPLOY_TYPE}`;
+var url = "localhost";
+var port = "8080";
+
+if (deployType !== "DEV") {
+  url = "sub.ybyblog.com";
+  port = "80";
+}
+
 app.get("/", (req, res) => {
-  res.send("Server is Running!");
+  res.send(`Server is Running! http://${url}:${port}`);
 });
 
-app.listen(PORT, "localhost", () => {
-  console.log(`App listening at http://localhost:${PORT}`);
+app.listen(port, url, () => {
+  console.log(`App listening at http://${url}:${port}`);
 });
